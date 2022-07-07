05e38425270af6be27c43f9a9da90f5bf7827a88
Hakan DİNÇTÜRK <58170470+SoFY111@users.noreply.github.com>
2022-07-07 16:37:35 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const { v4: uuidv4 } = require('uuid');
+
+const RABBITMQ = 'rabbitmqurl';
+
+const open = require('amqplib').connect(RABBITMQ);
+const q = 'AUTH_QUEUE';
+// Consumer
+open
+  .then(function (conn) {
+    console.log(`[ ${new Date()} ] Server started`);
+    return conn.createChannel();
+  })
+  .then(function (ch) {
+    return ch.assertQueue(q).then(function (ok) {
+      return ch.consume(q, function (msg) {
+        console.log(
+          `[ ${new Date()} ] Message received: ${JSON.stringify(
+            JSON.parse(msg.content.toString('utf8')),
+          )}`,
+        );
+        if (msg !== null) {
+          const response = {
+            uuid: uuidv4(),
+          };
+
+          console.log(
+            `[ ${new Date()} ] Message sent: ${JSON.stringify(response)}`,
+          );
+
+          ch.sendToQueue(
+            msg.properties.replyTo,
+            Buffer.from(JSON.stringify(response)),
+            {
+              correlationId: msg.properties.correlationId,
+            },
+          );
+
+          ch.ack(msg);
+        }
+      });
+    });
+  })
+  .catch(console.warn);
